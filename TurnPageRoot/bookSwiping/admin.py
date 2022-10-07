bc16784fc6da53da638fdbcd6ab8bcc0e3a89a64
sjeremycohen <sjeremycohen@gmail.com>
2022-10-07 14:06:00 -0400

    Add models to admin panel

--- TurnPageRoot/bookSwiping/admin.py
+++ TurnPageRoot/bookSwiping/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Book
+from .models import User
+from .models import Genre
+from .models import BookGenre
+from .models import UserGenre
+from .models import Bookshelf
