a0279c4369fad5db336cad74d5e8217b5d63413a
emoltz <es5541@nyu.edu>
2022-10-08 12:19:00 -0400

    small edits to pytest.ini

--- TurnPageRoot/bookSwiping/tests/test_views.py
+++ TurnPageRoot/bookSwiping/tests/test_views.py
@@ -1,6 +1,6 @@
 import pytest
 
+
 def test_home_endpoint_returns_welcome_page(client):
-	response = client.get(path='/')
-	assert response.status_code == 200
-	assert 'Welcome to TurnPage ! Heaven for Book Lovers' in str(response.content)
+    response = client.get(path='/')
+    assert response.status_code == 200
