3dc6583d6f3370573aab99cb51fab6ed4ef54163
emoltz <es5541@nyu.edu>
2022-10-08 17:58:04 -0400

    works for mobile

--- TurnPageRoot/static/js/script.js
+++ TurnPageRoot/static/js/script.js
@@ -1,7 +1,27 @@
 console.log("https://github.com/gcivil-nyu-org/team-3-inperson")
-$( function (){
-    $('.draggable').draggable();
+$(function () {
+    $('.draggable').draggable({
+        // axis: 'x',
+        cursor: "grabbing"
+    });
 });
+
+// $(document).ready(function () {
+//     $('.fade-out-btn').click(function () {
+//         $('.draggable').animate(
+//             {deg: 70},
+//             {duration: 700,
+//             step: function(now){
+//                 $(this).css({
+//                     transform:'rotate('+now+'deg)'
+//                 })
+//             }}
+//         ).fadeOut(700).promise();
+//
+//     });
+//
+// });
+
 
 // document.addEventListener('mousemove', (event) => {
 //     console.log(`(${event.clientX}, ${event.clientY})`);
