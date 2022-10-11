ba7515159c8a4b692cd6d30bc297fe3cbdded46f
emoltz <es5541@nyu.edu>
2022-10-10 20:14:52 -0400

    basic swiping animation

--- TurnPageRoot/static/js/script.js
+++ TurnPageRoot/static/js/script.js
@@ -2,28 +2,49 @@ console.log("https://github.com/gcivil-nyu-org/team-3-inperson")
 
 let startingPosition;
 let currentPosition;
+let swipedRight = false;
+let swipedLeft = false;
 
 $('.draggable').draggable({
+    data: {
+        startingPosition: startingPosition,
+        currentPosition: currentPosition,
+    },
+
     drag: function (e, ui) {
             startingPosition = ui.originalPosition;
             currentPosition = ui.position;
             // console.log(currentPosition);
             // TODO screen width calculation
-            if (currentPosition.left > 700) {
-                console.log("Swiped right")
+            if (currentPosition.left > 300) {
+                swipedRight = true;
+                // console.log("swipe right");
+
+
             }
-            else if (currentPosition.left < 50) {
-                console.log("Swiped left")
+            else if (currentPosition.left < -300) {
+                console.log("swipe left");
             }
-            else if (currentPosition.top > 500) {
-                console.log("Swiped down")
+            else if (currentPosition.top > 300) {
+                console.log("swipe down");
             }
+
     },
-    data: {
-        startingPosition: startingPosition,
-        currentPosition: currentPosition,
+    stop: function(e, ui) {
+        if(currentPosition.left > 300){
+            $('.draggable').draggable("option","revert", false).animate({left: 1000}, 300).css(
+                {'transform': 'rotate(20deg)'}
+            );
+        }
+        else if (currentPosition.left < -300){
+            $('.draggable').draggable("option","revert", false).animate({left: -1000}, 300).css(
+                {'transform': 'rotate(-20deg)'}
+            );
+        }
+
     },
-    cursor: "grabbing",
     revert: true,
+    cursor: "grabbing",
+
     revertDuration: 50,
 });
