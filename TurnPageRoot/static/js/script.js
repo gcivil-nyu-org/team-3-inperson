f4dc14fcbcd1371308f887df65729a1bd99b8c02
Ethan Shafran Moltz <33405530+emoltz@users.noreply.github.com>
2022-10-14 10:18:30 -0400

    #89 dragging animations (#99)
    * documentation
    
    * gets smaller
    BUG: container shrinks with the book
    
    * animations
    
    * animations work
    new bug: when swiping, we need to get the current amount rotated and keep it at that for the movement

--- TurnPageRoot/static/js/script.js
+++ TurnPageRoot/static/js/script.js
@@ -5,6 +5,11 @@ let currentPosition;
 let swipedRight = false;
 let swipedLeft = false;
 
+// VALUES
+let shrinkValue = 45;
+let rotateValue = 30;
+
+
 $('.draggable').draggable({
     data: {
         startingPosition: startingPosition,
@@ -12,39 +17,56 @@ $('.draggable').draggable({
     },
 
     drag: function (e, ui) {
-            startingPosition = ui.originalPosition;
-            currentPosition = ui.position;
-            // console.log(currentPosition);
-            // TODO screen width calculation
-            if (currentPosition.left > 300) {
-                swipedRight = true;
-                console.log("swipe right");
-                $('.draggable').draggable("option","revert", false);
+        startingPosition = ui.originalPosition;
+        currentPosition = ui.position;
+        // console.log(currentPosition);
+        // TODO screen width calculation
+
+        // BOOK ROTATES TOWARDS POSITION
+        $('.book-cover-img').css('transform', 'rotate(' + currentPosition.left / rotateValue + 'deg)')
+            .css('width', shrinkValue + '%')
+            .css('opacity', 1 - Math.abs(currentPosition.left / 700))
+
+        ;
 
 
-            }
-            else if (currentPosition.left < -300) {
-                console.log("swipe left");
-                $('.draggable').draggable("option","revert", false);
-            }
-            else if (currentPosition.top > 300) {
-                console.log("swipe down");
-                $('.draggable').draggable("option","revert", false);
-            }
+        //WHEN SWIPING, MAKE SURE IT DOESN'T SNAP BACK
+        if (currentPosition.left > 300) {
+            swipedRight = true;
+            console.log("swipe right");
+            $('.draggable').draggable("option", "revert", false);
+
+
+        } else if (currentPosition.left < -300) {
+            console.log("swipe left");
+            $('.draggable').draggable("option", "revert", false);
+        } else if (currentPosition.top > 300) {
+            console.log("swipe down");
+            $('.draggable').draggable("option", "revert", false);
+        }
 
     },
-    stop: function(e, ui) {
-        if(currentPosition.left > 300){
-            $('.draggable').animate({left: 1000}, 300).css(
-                {'transform': 'rotate(20deg)'}
-            ).hide("fade",{percent:0}, 150);
-        }
-        else if (currentPosition.left < -300){
-            $('.draggable').animate({left: -1000}, 300).css(
-                {'transform': 'rotate(-20deg)'}
-            ).hide("fade",{percent:0}, 150);
-        }
-        else if(currentPosition.top > 300){
+    stop: function (e, ui) {
+        // RESET ROTATION
+        $('.book-cover-img').css('transform', 'rotate(0deg)')
+            .css('width', '50%')
+            .css('opacity', 100)
+        ;
+
+
+        // LISTENERS FOR SWIPING ACTION
+        if (currentPosition.left > 300) {
+            $('.book-cover-img').css('width', shrinkValue + '%');
+            $('.draggable').animate({left: 1000}, 300)
+                .css({'transform': 'rotate(20deg)'})
+                .hide("fade", {percent: 0}, 150);
+        } else if (currentPosition.left < -300) {
+            $('.book-cover-img').css('width', shrinkValue + '%');
+            $('.draggable').animate({left: -1000}, 300)
+                .css({'transform': 'rotate(-20deg)'})
+                .hide("fade", {percent: 0}, 150);
+        } else if (currentPosition.top > 300) {
+            $('.book-cover-img').css('width', shrinkValue + '%');
             $('.draggable').hide("scale", {percent: 0}, 150);
         }
 
