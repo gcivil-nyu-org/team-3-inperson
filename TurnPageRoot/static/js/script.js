b233a559a43c17ecdd8fa0f6f37dd7fec75439d7
Michael Zachor <mfz226@nyu.edu>
2022-10-15 16:30:53 -0400

    mobile swiping working

--- TurnPageRoot/static/js/script.js
+++ TurnPageRoot/static/js/script.js
@@ -6,9 +6,15 @@ let swipedRight = false;
 let swipedLeft = false;
 
 // VALUES
-let shrinkValue = 45;
-let rotateValue = 30;
+const bookWidth = screen.width > 991 ? 50 : (screen.width > 600 ? 70 : 90);
 
+const shrinkValue = bookWidth * .9;
+const rotateValue = 30;
+
+const leftSwipeCutoffPoint = screen.width / 5;
+const rightSwipeCutoffPoint = screen.width / (5/4);
+const horizontalSwipeCutoffPoint = screen.width / 5;
+const downSwipeCutoffPoint = screen.height / 7;
 
 $('.draggable').draggable({
     data: {
@@ -19,8 +25,6 @@ $('.draggable').draggable({
     drag: function (e, ui) {
         startingPosition = ui.originalPosition;
         currentPosition = ui.position;
-        // console.log(currentPosition);
-        // TODO screen width calculation
 
         // BOOK ROTATES TOWARDS POSITION
         $('.book-cover-img').css('transform', 'rotate(' + currentPosition.left / rotateValue + 'deg)')
@@ -29,18 +33,18 @@ $('.draggable').draggable({
 
         ;
 
-
         //WHEN SWIPING, MAKE SURE IT DOESN'T SNAP BACK
-        if (currentPosition.left > 300) {
+        console.log(screen.width, currentPosition.left)
+        if (currentPosition.left > horizontalSwipeCutoffPoint) {
             swipedRight = true;
             console.log("swipe right");
             $('.draggable').draggable("option", "revert", false);
 
 
-        } else if (currentPosition.left < -300) {
+        } else if (currentPosition.left < -1*horizontalSwipeCutoffPoint) {
             console.log("swipe left");
             $('.draggable').draggable("option", "revert", false);
-        } else if (currentPosition.top > 300) {
+        } else if (currentPosition.top > downSwipeCutoffPoint) {
             console.log("swipe down");
             $('.draggable').draggable("option", "revert", false);
         }
@@ -49,23 +53,23 @@ $('.draggable').draggable({
     stop: function (e, ui) {
         // RESET ROTATION
         $('.book-cover-img').css('transform', 'rotate(0deg)')
-            .css('width', '50%')
+            .css('width', bookWidth+'%')
             .css('opacity', 100)
         ;
 
 
         // LISTENERS FOR SWIPING ACTION
-        if (currentPosition.left > 300) {
+        if (currentPosition.left > horizontalSwipeCutoffPoint) {
             $('.book-cover-img').css('width', shrinkValue + '%');
             $('.draggable').animate({left: 1000}, 300)
                 .css({'transform': 'rotate(20deg)'})
                 .hide("fade", {percent: 0}, 150);
-        } else if (currentPosition.left < -300) {
+        } else if (currentPosition.left < -1*horizontalSwipeCutoffPoint) {
             $('.book-cover-img').css('width', shrinkValue + '%');
             $('.draggable').animate({left: -1000}, 300)
                 .css({'transform': 'rotate(-20deg)'})
                 .hide("fade", {percent: 0}, 150);
-        } else if (currentPosition.top > 300) {
+        } else if (currentPosition.top > downSwipeCutoffPoint) {
             $('.book-cover-img').css('width', shrinkValue + '%');
             $('.draggable').hide("scale", {percent: 0}, 150);
         }
