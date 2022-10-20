f045e09b152b4fb5078947b91f61dbc8dffdd09c
emoltz <es5541@nyu.edu>
2022-10-20 17:01:55 -0400

    tweaks to admin

--- TurnPageRoot/bookSwiping/admin.py
+++ TurnPageRoot/bookSwiping/admin.py
@@ -12,20 +12,14 @@ from .models import Bookshelf
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    # list_display = ['title', 'author', 'published_date', 'date_created', 'isbn10', 'isbn13']
-    # list_filter = ['published_date', 'date_created']
+    list_display = ['title', 'author', 'published_date', 'isbn10', 'isbn13']
+    list_filter = ['published_date']
     search_fields = ['title', 'author', 'isbn10', 'isbn13']
     date_hierarchy = 'published_date'
-    # ordering = ['published_date', 'date_created']
-
-
-# @admin.register(User)
-# class UserAdmin(admin.ModelAdmin):
-#     list_display = ['name', 'email']
-#     search_fields = ['name', 'email']
 
 
 admin.site.register(Genre)
 admin.site.register(BookGenre)
 admin.site.register(UserGenre)
 admin.site.register(Bookshelf)
+admin.site.register(User)
