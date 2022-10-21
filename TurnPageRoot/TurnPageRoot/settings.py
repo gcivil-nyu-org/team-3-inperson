6d0bdd1b7951bf55d8b6d05044dff7ced0c69f02
emoltz <es5541@nyu.edu>
2022-10-21 12:43:54 -0400

    old database stuff in here

--- TurnPageRoot/TurnPageRoot/settings.py
+++ TurnPageRoot/TurnPageRoot/settings.py
@@ -98,11 +98,11 @@ else:
     # DATABASES = {
     #     'default': {
     #         'ENGINE': 'django.db.backends.postgresql',
-    #         'NAME': 'travis_ci_test',
-    #         'USER': 'postgres',
-    #         'PASSWORD': '',
-    #         'HOST': '127.0.0.1',
-    #         'PORT': '5432',
+    #         'NAME': env('DB_NAME'),
+    #         'USER': env('DB_USER'),
+    #         'PASSWORD': env('PASSWORD'),
+    #         'HOST': env('HOSTNAME'),
+    #         'PORT': env('PORT')
     #     }
     # }
     DATABASES = {
