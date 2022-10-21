5415ba44fe09d16f5ea8604281dd931145649710
emoltz <es5541@nyu.edu>
2022-10-21 10:02:43 -0400

    removed unused import statement

--- TurnPageRoot/TurnPageRoot/settings.py
+++ TurnPageRoot/TurnPageRoot/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import django
 import os
 import environ
-import psycopg2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
