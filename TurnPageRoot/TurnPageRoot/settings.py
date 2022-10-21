76648fe0a7afcdedceabd9b4f8a9b385cfe31fc0
emoltz <es5541@nyu.edu>
2022-10-21 15:22:30 -0400

    trying to fix css

--- TurnPageRoot/TurnPageRoot/settings.py
+++ TurnPageRoot/TurnPageRoot/settings.py
@@ -133,9 +133,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles",
+    BASE_DIR / "static",
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
