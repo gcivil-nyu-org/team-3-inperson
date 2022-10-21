c00f0c2e8017e7514cce90ccda0f5cfec50e906a
emoltz <es5541@nyu.edu>
2022-10-21 12:39:26 -0400

    sqlite

--- TurnPageRoot/TurnPageRoot/settings.py
+++ TurnPageRoot/TurnPageRoot/settings.py
@@ -93,24 +93,24 @@ if 'RDS_DB_NAME' in os.environ:  # RDS database
     }
 else:
     # Connect to the same RDS instance but the credentials are local:
-    env = environ.Env()
-    environ.Env.read_env()
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'travis_ci_test',
-            'USER': 'postgres',
-            'PASSWORD': '',
-            'HOST': '127.0.0.1',
-            'PORT': '5432',
-        }
-    }
+    # env = environ.Env()
+    # environ.Env.read_env()
     # DATABASES = {
     #     'default': {
-    #         'ENGINE': 'django.db.backends.sqlite3',
-    #         'NAME': BASE_DIR / 'db.sqlite3',
+    #         'ENGINE': 'django.db.backends.postgresql',
+    #         'NAME': 'travis_ci_test',
+    #         'USER': 'postgres',
+    #         'PASSWORD': '',
+    #         'HOST': '127.0.0.1',
+    #         'PORT': '5432',
     #     }
     # }
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
