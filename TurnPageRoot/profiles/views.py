86e3d409f49f79bffece89c1e30ca5d3baded724
emoltz <es5541@nyu.edu>
2022-10-22 16:15:16 -0400

    user profile working

--- TurnPageRoot/profiles/views.py
+++ TurnPageRoot/profiles/views.py
@@ -39,6 +39,11 @@ class MyBookshelf(LoginRequiredMixin, TemplateView):
 class UserProfile(LoginRequiredMixin, TemplateView):
     template_name = "profiles/user_profile.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        return context
+
 
 class LogoutInterfaceView(LogoutView):
     template_name = "profiles/logout.html"
