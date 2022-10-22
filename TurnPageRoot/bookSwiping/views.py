eedf3d86fa3fec4e1236a4f0486e8ac7857d19c3
emoltz <es5541@nyu.edu>
2022-10-22 15:35:50 -0400

    black reformatting

--- TurnPageRoot/bookSwiping/views.py
+++ TurnPageRoot/bookSwiping/views.py
@@ -8,7 +8,7 @@ import random
 class HomeView(ListView):
     model = Book
     context_object_name = "books"
-    template_name = 'bookSwiping/home.html'
+    template_name = "bookSwiping/home.html"
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -16,17 +16,16 @@ class HomeView(ListView):
         # change to how many random items you want
         random_items = random.sample(items, 2)
 
-        context['top_book'] = random_items[0]
-        context['on_deck'] = random_items[1]
+        context["top_book"] = random_items[0]
+        context["on_deck"] = random_items[1]
         return context
 
 
-
 class LoginView(TemplateView):
-    template_name = 'bookSwiping/login.html'
+    template_name = "bookSwiping/login.html"
     extra_context = {}
 
 
 class SignupView(TemplateView):
-    template_name = 'bookSwiping/signup.html'
+    template_name = "bookSwiping/signup.html"
     extra_context = {}
