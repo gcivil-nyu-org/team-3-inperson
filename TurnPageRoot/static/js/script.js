ca81ecc46629bb6b45786c53ac96f737ce633a22
emoltz <es5541@nyu.edu>
2022-10-23 15:43:46 -0400

    stack is swipable but whole stack moves when things change direction

--- TurnPageRoot/static/js/script.js
+++ TurnPageRoot/static/js/script.js
@@ -3,6 +3,7 @@ let startingPosition;
 let currentPosition;
 let swipedRight = false;
 let swipedLeft = false;
+let counter = 1;
 
 // VALUES
 const bookWidth = screen.width > 991 ? 50 : (screen.width > 600 ? 70 : 90);
@@ -15,6 +16,8 @@ const rightSwipeCutoffPoint = screen.width / (5 / 4);
 const horizontalSwipeCutoffPoint = screen.width / 5;
 const downSwipeCutoffPoint = screen.height / 7;
 let bookshelfMoveValue = screen.width > 991 ? 400 : (screen.width > 600 ? 300 : 100);
+
+// TESTS
 
 
 //FUNCTIONS
@@ -34,67 +37,81 @@ function swipedDownAnimation() {
     $('.draggable').hide("scale", {percent: 0}, 150);
 }
 
+function nextBook() {
+    counter++;
+    // this will have to switch the classes in the HTML so that the next book is draggable until the stack is empty
+    $('#book' + counter).addClass('draggable');
+    $('#book' + counter + '-img').addClass('top-of-stack');
+    makeDraggable();
+}
 
-$('.draggable').draggable({
-    data: {
-        startingPosition: startingPosition,
-        currentPosition: currentPosition,
-    },
+function makeDraggable() {
+    $('.draggable').draggable({
+        data: {
+            startingPosition: startingPosition,
+            currentPosition: currentPosition,
+        },
 
-    drag: function (e, ui) {
-        startingPosition = ui.originalPosition;
-        currentPosition = ui.position;
+        drag: function (e, ui) {
+            startingPosition = ui.originalPosition;
+            currentPosition = ui.position;
 
-        // BOOK ROTATES TOWARDS POSITION
-        $('.top-of-stack').css('transform', 'rotate(' + currentPosition.left / rotateValue + 'deg)')
-            .css('width', shrinkValue + '%')
-            .css('opacity', 1 - Math.abs(currentPosition.left / 700))
+            // BOOK ROTATES TOWARDS POSITION
+            $('.top-of-stack').css('transform', 'rotate(' + currentPosition.left / rotateValue + 'deg)')
+                .css('width', shrinkValue + '%')
+                .css('opacity', 1 - Math.abs(currentPosition.left / 700))
 
-        ;
+            ;
 
-        //WHEN SWIPING, MAKE SURE IT DOESN'T SNAP BACK
-        // console.log(screen.width, currentPosition.left)
-        if (currentPosition.left > horizontalSwipeCutoffPoint) {
-            swipedRight = true;
-            console.log("swipe right");
-            $('.draggable').draggable("option", "revert", false);
-
-
-        } else if (currentPosition.left < -1 * horizontalSwipeCutoffPoint) {
-            // console.log("swipe left");
-            $('.draggable').draggable("option", "revert", false);
-        } else if (currentPosition.top > downSwipeCutoffPoint) {
-            // console.log("swipe down");
-            $('.draggable').draggable("option", "revert", false);
-        }
-
-    },
-    stop: function (e, ui) {
-        // RESET ROTATION
-        $('.top-of-stack').css('transform', 'rotate(0deg)')
-            .css('width', bookWidth + '%')
-            .css('opacity', 100)
-        ;
+            //WHEN SWIPING, MAKE SURE IT DOESN'T SNAP BACK
+            // console.log(screen.width, currentPosition.left)
+            if (currentPosition.left > horizontalSwipeCutoffPoint) {
+                swipedRight = true;
+                console.log("swipe right");
+                $('.draggable').draggable("option", "revert", false);
 
 
-        // LISTENERS FOR SWIPING ACTION
-        if (currentPosition.left > horizontalSwipeCutoffPoint) {
-            $('.top-of-stack').css('width', shrinkValue + '%');
-            swipedRightAnimation();
-        } else if (currentPosition.left < -1 * horizontalSwipeCutoffPoint) {
-            $('.top-of-stack').css('width', shrinkValue + '%');
-            swipedLeftAnimation()
-        } else if (currentPosition.top > downSwipeCutoffPoint) {
-            $('.top-of-stack').css('width', shrinkValue + '%');
-            swipedDownAnimation();
-        }
+            } else if (currentPosition.left < -1 * horizontalSwipeCutoffPoint) {
+                // console.log("swipe left");
+                $('.draggable').draggable("option", "revert", false);
+            } else if (currentPosition.top > downSwipeCutoffPoint) {
+                // console.log("swipe down");
+                $('.draggable').draggable("option", "revert", false);
+            }
 
-    },
-    revert: true,
-    cursor: "grabbing",
+        },
+        stop: function (e, ui) {
+            // RESET ROTATION
+            $('.top-of-stack').css('transform', 'rotate(0deg)')
+                .css('width', bookWidth + '%')
+                .css('opacity', 100)
+            ;
 
-    revertDuration: 50,
-});
+
+            // LISTENERS FOR SWIPING ACTION
+            if (currentPosition.left > horizontalSwipeCutoffPoint) {
+                $('.top-of-stack').css('width', shrinkValue + '%');
+                swipedRightAnimation();
+                nextBook();
+            } else if (currentPosition.left < -1 * horizontalSwipeCutoffPoint) {
+                $('.top-of-stack').css('width', shrinkValue + '%');
+                swipedLeftAnimation()
+                nextBook();
+            } else if (currentPosition.top > downSwipeCutoffPoint) {
+                $('.top-of-stack').css('width', shrinkValue + '%');
+                swipedDownAnimation();
+                nextBook();
+            }
+
+        },
+        revert: true,
+        cursor: "grabbing",
+
+        revertDuration: 50,
+    });
+}
+
+makeDraggable();
 
 
 $('#swipe-right-btn').click(function () {
@@ -107,6 +124,6 @@ $('#swipe-left-btn').click(function () {
 
 
 $('#bookshelf-btn').click(function () {
-    $('.draggable').animate({top:bookshelfMoveValue + 'px'}, 200)
+    $('.draggable').animate({top: bookshelfMoveValue + 'px'}, 200)
     swipedDownAnimation();
 });
