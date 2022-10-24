328f9a1e95e95d4adde917de9630858881cb8ff9
emoltz <es5541@nyu.edu>
2022-10-24 09:35:46 -0400

    testing models

--- TurnPageRoot/bookSwiping/tests/test_models.py
+++ TurnPageRoot/bookSwiping/tests/test_models.py
@@ -1,1 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .. import models
+
+
+class TestModels(TestCase):
+    object_list = []
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='jacob', email='jacob@…', password='top_secret')
+
+    def test_user(self):
+        self.assertEqual(self.user.username, 'jacob')
+
+
--- TurnPageRoot/bookSwiping/tests/test_views.py
+++ TurnPageRoot/bookSwiping/tests/test_views.py
@@ -1,29 +1,35 @@
-from django.test import TestCase, Client
+from django.test import TestCase, Client, RequestFactory
+from django.contrib.auth.models import User
+from .. import views
 from django.urls import reverse
 
 
-# class TestViews(TestCase):
-#
-#     def setUp(self):
-#         self.client = Client()
-#         self.home_url = reverse('home')
-#         self.login_url = reverse('login')
-#         self.signup_url = reverse('signup')
-#
-#     def test_project_list(self):
-#         response = self.client.get(self.home_url)
-#
-#         self.assertEquals(response.status_code, 200)
-#         self.assertTemplateUser(response, 'bookSwiping/home.html')
-#
-#     def test_project_list(self):
-#         response = self.client.get(self.login_url)
-#
-#         self.assertEquals(response.status_code, 200)
-#         self.assertTemplateUser(response, 'bookSwiping/login.html')
-#
-#     def test_project_list(self):
-#         response = self.client.get(self.signup_url)
-#
-#         self.assertEquals(response.status_code, 200)
-#         self.assertTemplateUser(response, 'bookSwiping/signup.html')
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.home_url = reverse('home')
+        self.login_url = reverse('login')
+        self.signup_url = reverse('signup')
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username='jacob', email='jacob@dhg.com', password='top_secret')
+
+    # TODO these are the ones that are failing
+    # def test_project_list(self):
+    #     response = self.client.get(self.home_url)
+    #
+    #     self.assertEquals(response.status_code, 200)
+    #     self.assertTemplateUser(response, 'bookSwiping/home.html')
+    #
+    # def test_project_list(self):
+    #     response = self.client.get(self.login_url)
+    #
+    #     self.assertEquals(response.status_code, 200)
+    #     self.assertTemplateUser(response, 'bookSwiping/login.html')
+    #
+    # def test_project_list(self):
+    #     response = self.client.get(self.signup_url)
+    #
+    #     self.assertEquals(response.status_code, 200)
+    #     self.assertTemplateUser(response, 'bookSwiping/signup.html')
