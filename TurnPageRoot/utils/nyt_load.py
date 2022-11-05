464e583e1ab87f97e04726d8f1475d3838d720e7
sjeremycohen <sjeremycohen@gmail.com>
2022-11-05 12:55:09 -0400

    Created base functions for NYTAPI, started file for cronjob

---
+++ TurnPageRoot/utils/nyt_load.py
@@ -0,0 +1,4 @@
+from utils.nytimes_api import *
+from bookSwiping.models import Book
+
+def loadBooks(date="current"):
---
+++ TurnPageRoot/utils/nytimes_api.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+from urllib.error import HTTPError
+import urllib.request
+import json
+import environ
+import os
+
+
+class NYTAPI:
+    url_base = "https://api.nytimes.com/svc/books/v3"
+    default_date = "current"
+    if "RDS_DB_NAME" in os.environ:
+        api_key = os.environ["NYT_API_KEY"]
+    else:
+        env = environ.Env()
+        environ.Env.read_env()
+        api_key = env("NYT_API_KEY")
+
+    @classmethod
+    def query_nyt(self, url):
+        return json.loads(urllib.request.urlopen(url).read())["results"]
+
+    @classmethod
+    def get_lists(self, date=default_date):
+        if date != self.default_date:
+            date = datetime.strptime(date, "%Y-%m-%d")
+        url = self.url_base + "names.json?api-key=" + self.api_key
+        try:
+            data = self.query_nyt(url)
+        except HTTPError:
+            return 1
+        else:
+            for result in range(20):  # uses a range because we need to delete the index
+                if datetime.strptime(
+                    data[result]["newest_published_date"], "%Y-%m-%d"
+                ) < datetime.strptime(date, "%Y-%m-%d"):
+                    del data[result]
+            return data
+
+    @classmethod
+    def get_books(
+        self, list, date=default_date
+    ):  # founding date of the NYT by default :)
+        if date != self.default_date:
+            date = datetime.strptime(date, "%Y-%m-%d")
+        url = self.url_base + date + "/" + list + ".json?api-key=" + self.api_key
+        try:
+            data = self.query_nyt(url)
+        except HTTPError:
+            return 1
+        else:
+            return data
