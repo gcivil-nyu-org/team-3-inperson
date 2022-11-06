6ee567705a4401ef9a910cb358ff72520d48cc64
sjeremycohen <sjeremycohen@gmail.com>
2022-11-06 14:23:25 -0500

    flake8

--- TurnPageRoot/utils/db_functions.py
+++ TurnPageRoot/utils/db_functions.py
@@ -1,5 +1,4 @@
 # from django.core.management.base import BaseCommand, CommandError
-from django.core.exceptions import ObjectDoesNotExist
 from django.db import IntegrityError
 from bookSwiping.models import Book, User, Bookshelf
 
