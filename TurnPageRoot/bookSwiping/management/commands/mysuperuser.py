2c779b300edb817f75d71f693de7da070557abbc
emoltz <es5541@nyu.edu>
2022-11-09 11:29:40 -0500

    quick fix to superuser command

--- TurnPageRoot/bookSwiping/management/commands/mysuperuser.py
+++ TurnPageRoot/bookSwiping/management/commands/mysuperuser.py
@@ -1,13 +1,13 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
-from ....profiles import models
+from profiles.models import Profile
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "myuser@myemail.com", "mypassword")
-            models.Profile.objects.create(user=User.objects.get(username="admin"), email_confirmed=True)
+        if not User.objects.filter(username="superuser").exists():
+            User.objects.create_superuser("superuser", "myuser@myemail.com", "mypassword")
+            Profile.objects.create(user=User.objects.get(username="superuser"), email_confirmed=True)
             print("Superuser created")
         else:
             print("Custom superuser already exists")
