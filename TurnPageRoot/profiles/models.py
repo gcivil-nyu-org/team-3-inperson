5097c09db095ba86a846c40357c6c157620b2a08
Pankhurinyu <pp2535@nyu.edu>
2022-11-15 20:15:57 -0500

    now the system will take only one email ID

--- TurnPageRoot/profiles/models.py
+++ TurnPageRoot/profiles/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+User._meta.get_field("email")._unique = True
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
