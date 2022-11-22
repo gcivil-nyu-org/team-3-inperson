1933cc2be3dddb71f9f7f7bdcf5d46524497d8ca
Pankhurinyu <pp2535@nyu.edu>
2022-11-22 11:15:50 -0500

    cleaned settings.py

--- TurnPageRoot/TurnPageRoot/settings.py
+++ TurnPageRoot/TurnPageRoot/settings.py
@@ -218,18 +218,12 @@ HASHIDS = {
 
 AUTHENTICATION_BACKENDS = (
     "social_core.backends.github.GithubOAuth2",
-    # 'social_core.backends.twitter.TwitterOAuth',
-    # 'social_core.backends.facebook.FacebookOAuth2',
     "social_core.backends.google.GoogleOAuth2",
     "social_core.backends.amazon.AmazonOAuth2",
     "django.contrib.auth.backends.ModelBackend",
 )
 
-# SOCIAL_AUTH_GITHUB_KEY = ''
-# SOCIAL_AUTH_GITHUB_SECRET = ''
-
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = env("GOOGLEKEY")
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = env("GOOGLESECRET")
 
-# SOCIAL_AUTH_AMAZON_KEY = env("AmazonSECRET")
-# SOCIAL_AUTH_AMAZON_SECRET = env("AmazonSECRET")
+
